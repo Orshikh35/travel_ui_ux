cfa9986d18ea6954d699787bf7450daec5dd4d18
Bat Orshikh <batorshikh@Bats-MacBook-Pro.local>
2024-05-14 00:59:02 +0800

    d

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,15 @@ import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Logo from "../assets/Logo.jpg"
 
 const Footer = () => {
   return (
-    <footer className="flexCenter mb-24">
+    <footer className="flexCenter mb-24 mt-20">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image src="hilink-logo.svg" alt="logo" width={74} height={29}/>
+            <img src={Logo.src} alt="" className='h-[30px]' />
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
@@ -46,7 +47,7 @@ const Footer = () => {
 
             <div className="flex flex-col gap-5">
               <FooterColumn title={SOCIALS.title}>
-                <ul className="regular-14 flex gap-4 text-gray-30">
+                <ul className="regular-14 flex gap-4 text-gray-30 items-center">
                   {SOCIALS.links.map((link) => (
                     <Link href="/" key={link}>
                       <Image src={link} alt="logo" width={24} height={24} />
@@ -58,8 +59,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="border bg-gray-20" />
-        <p className="regular-14 w-full text-center text-gray-30">2023 Hilink | All rights reserved</p>
+        
       </div>
     </footer>
   )
--- constants/index.ts
+++ constants/index.ts
@@ -1,10 +1,10 @@
 // NAVIGATION
 export const NAV_LINKS = [
-  { href: '/', key: 'home', label: 'Home' },
-  { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
-  { href: '/', key: 'services', label: 'Services' },
-  { href: '/', key: 'pricing ', label: 'Pricing ' },
-  { href: '/', key: 'contact_us', label: 'Contact Us' },
+  { href: '/', key: 'home', label: 'Танилцуулга' },
+  { href: '/', key: 'how_hilink_work', label: 'Үйл ажиллагаа' },
+  { href: '/', key: 'services', label: 'Төсөл' },
+  { href: '/', key: 'pricing ', label: 'Холбоо барих' },
+  { href: '/', key: 'contact_us', label: '' },
 ];
 
 // CAMP SECTION
@@ -50,27 +50,16 @@ export const FEATURES = [
 // FOOTER SECTION
 export const FOOTER_LINKS = [
   {
-    title: 'Learn More',
-    links: [
-      'About Hilink',
-      'Press Releases',
-      'Environment',
-      'Jobs',
-      'Privacy Policy',
-      'Contact Us',
-    ],
-  },
-  {
     title: 'Our Community',
     links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
   },
 ];
 
 export const FOOTER_CONTACT_INFO = {
-  title: 'Contact Us',
+  title: 'Холбоо барих',
   links: [
-    { label: 'Admin Officer', value: '123-456-7890' },
-    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    { label: 'Дугаар', value: '+976 88105001' },
+    { label: 'И-мейл', value: 'Shidbuteel@gmail.com' },
   ],
 };
 
